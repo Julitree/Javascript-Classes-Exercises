89e593489325265e16d8a770f64bb25895921883
julytree@hotmail.es <Dulcecafe1981>
2020-03-21 10:12:11 +0100

    fixed

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
- //#1 Evaluate these: [2] === [2] ; {} === {} 
-
-
-
+//Evaluate these:
+//#1
+[2] === [2] // false
+{} === {} // false
 
 //#2 what is the value of property a for each object.
-const object1 = { a: 5 }; 
-const object2 = object1; 
-const object3 = object2; 
-const object4 = { a: 5}; 
+const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; // 5
 object1.a = 4;
 
 
 //#3 create two classes: an Animal class and a Mamal class. 
-// create a cow that accepts a name, type and color and has a 
-//sound method that moo's her name, type and color. 
-
-class Animal{
-	constuctor(name, type, color){
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal {
+	constructor(name, type, color) {
 		this.name = name;
+		this.color = color;
 		this.type = type;
-		this.color = color; 
 	}
 }
 
-class Mamal extends anima{
-	super(name, type, color){
-
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
 	}
 }
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
