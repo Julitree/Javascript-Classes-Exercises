1b4f2aaf9867f91f981c4d6d0b1a0296afcf0ab5
julytree@hotmail.es <Dulcecafe1981>
2020-07-08 15:36:44 +0200

    Udemy_JSS

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-
-//1 create two classes: an Animal class and a Mamal class. 
-// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
-class Animal {
-	constructor(name, type, color) {
-		this.name = name;
-		this.color = color;
-		this.type = type;
-	}
-}
-
-class Mamal extends Animal {
-	constructor(name, type, color) {
-		super(name, type, color)
-	}
-	sound() {
-		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
-	}
-}
-
-const cow = new Mamal('Shelly', 'cow', 'brown');
+function logReset(event) {
+	log.textContent = `Form reset! Time stamp`;
+  }
+  
+  const form = document.getElementById('form');
+  const log = document.getElementById('log');
+  form.addEventListener('reset', logReset);
